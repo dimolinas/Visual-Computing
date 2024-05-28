49cd215782cca39677d5419644781c2abe240b7a
Diego Molina <dimolinas@unal.edu.co>
2024-05-27 20:58:51 -0500

    load model and init main class

--- portalScene/src/script.js
+++ portalScene/src/script.js
@@ -15,7 +15,6 @@ const gui = new dat.GUI({
     width: 400
 });
 
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
@@ -34,6 +33,13 @@ dracoLoader.setDecoderPath('draco/');
 const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 
+//Model
+gltfLoader.load(
+    './portal.glb',
+    (gltf) => {
+        scene.add(gltf.scene);
+    }
+)
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -48,11 +54,39 @@ const sizes = {
     height: window.innerHeight
 }
 
-// Camera
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-camera.position.z = 2;
-scene.add(camera);
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+  
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+  
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
 
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+    45,
+    sizes.width / sizes.height,
+    0.1,
+    100
+);
+
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 4;
+scene.add(camera);
+  
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 
 // Renderer
@@ -62,10 +96,26 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
 
 
